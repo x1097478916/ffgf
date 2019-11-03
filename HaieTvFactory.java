a1bb289ff125eb74d5439cfa1a349ac444915fc5
X1097478916 <1097478916@qq.com>
2019-11-03 11:57:07 +0800

    9/11/3

--- HaieTvFactory.java
+++ HaieTvFactory.java
@@ -5,3 +5,29 @@ import xxx.tv.*;
 		return new HaieTv();
 	}
 }
+package utility;
+import javax.xml.parsers.*;
+	import org.w3c.dom.*;
+	import org.xml.sax.SAXException;
+	import java.io.*;
+	public class XMLUtility{
+		public static Object getBean(){
+			try{
+				DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = dFactory.newDocumentBuilder();
+				Document doc = builder.parse(new File("config.xml"));
+				NodeList nl = doc.getElementsByTagName("alassName");
+				Node classNode = nl.item(0).getFirstChild();
+				String cName = classNode.getNodeValue();
+				Class c = Class.forName(cName);
+				Object obj =c.newInstance();
+				return obj;
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+				return null;
+			}
+		}
+	}
+
+
+
